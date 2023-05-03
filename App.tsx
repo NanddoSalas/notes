9a23c0b2339ddcebbc4850f59f8928676a438451
Luis Fernando Cano Salas <nanddosalas@gmail.com>
2023-05-03 13:38:50 -0700

    make storage persistent

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
 import { polyfillWebCrypto } from 'expo-standard-web-crypto';
+import { useEffect } from 'react';
 import { Navigation } from './src/Navigation';
+import { useNotes } from './src/hooks/useNotes';
 
 polyfillWebCrypto();
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const hasHydrated = useNotes((state) => state._hasHydrated);
+
+  useEffect(() => {
+    if (hasHydrated) {
+      SplashScreen.hideAsync();
+    }
+  }, [hasHydrated]);
+
   return (
     <NavigationContainer>
       <Navigation />
--- src/hooks/useNotes.tsx
+++ src/hooks/useNotes.tsx
@@ -1,9 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 import { Note } from '../types';
 
 interface NotesStore {
+  _hasHydrated: boolean;
   notes: Note[];
   selectedNotes: string[];
+  setHasHydrated: (value: boolean) => void;
   addNote: (note: Note) => void;
   updateNote: (noteId: string, title: string, text: string) => void;
   deleteNote: (noteId: string) => void;
@@ -13,57 +17,80 @@ interface NotesStore {
   deleteSelectedNotes: () => void;
 }
 
-export const useNotes = create<NotesStore>((set, get) => ({
-  notes: [],
-  selectedNotes: [],
-  addNote: (note) => set((state) => ({ notes: [...state.notes, note] })),
-  updateNote: (noteId, title, text) =>
-    set((state) => ({
-      notes: state.notes.map((item) => {
-        if (item.id === noteId) {
-          return { ...item, title, text };
-        }
+export const useNotes = create<NotesStore>()(
+  persist(
+    (set, get) => ({
+      _hasHydrated: false,
+      notes: [],
+      selectedNotes: [],
+      setHasHydrated: (value) => {
+        set({
+          _hasHydrated: value,
+        });
+      },
+      addNote: (note) => set((state) => ({ notes: [...state.notes, note] })),
+      updateNote: (noteId, title, text) =>
+        set((state) => ({
+          notes: state.notes.map((item) => {
+            if (item.id === noteId) {
+              return { ...item, title, text };
+            }
 
-        return item;
-      }),
-    })),
-  deleteNote: (noteId) =>
-    set((state) => ({
-      notes: state.notes.filter((item) => item.id !== noteId),
-    })),
-  getNote: (noteId) => get().notes.find((item) => item.id === noteId),
-  handleSelection: (noteId) =>
-    set((state) => {
-      const isSelected = state.selectedNotes.find((item) => item === noteId);
+            return item;
+          }),
+        })),
+      deleteNote: (noteId) =>
+        set((state) => ({
+          notes: state.notes.filter((item) => item.id !== noteId),
+        })),
+      getNote: (noteId) => get().notes.find((item) => item.id === noteId),
+      handleSelection: (noteId) =>
+        set((state) => {
+          const isSelected = state.selectedNotes.find(
+            (item) => item === noteId,
+          );
 
-      if (isSelected) {
-        return {
-          ...state,
-          selectedNotes: state.selectedNotes.filter((item) => item !== noteId),
-          notes: state.notes.map((note) =>
-            note.id === noteId ? { ...note, isSelected: !isSelected } : note,
-          ),
-        };
-      }
+          if (isSelected) {
+            return {
+              ...state,
+              selectedNotes: state.selectedNotes.filter(
+                (item) => item !== noteId,
+              ),
+              notes: state.notes.map((note) =>
+                note.id === noteId
+                  ? { ...note, isSelected: !isSelected }
+                  : note,
+              ),
+            };
+          }
 
-      return {
-        ...state,
-        selectedNotes: [...state.selectedNotes, noteId],
-        notes: state.notes.map((note) =>
-          note.id === noteId ? { ...note, isSelected: !isSelected } : note,
-        ),
-      };
+          return {
+            ...state,
+            selectedNotes: [...state.selectedNotes, noteId],
+            notes: state.notes.map((note) =>
+              note.id === noteId ? { ...note, isSelected: !isSelected } : note,
+            ),
+          };
+        }),
+      deselectNotes: () =>
+        set((store) => ({
+          ...store,
+          selectedNotes: [],
+          notes: store.notes.map((note) => ({ ...note, isSelected: false })),
+        })),
+      deleteSelectedNotes: () =>
+        set((store) => ({
+          ...store,
+          selectedNotes: [],
+          notes: store.notes.filter((note) => !note.isSelected),
+        })),
     }),
-  deselectNotes: () =>
-    set((store) => ({
-      ...store,
-      selectedNotes: [],
-      notes: store.notes.map((note) => ({ ...note, isSelected: false })),
-    })),
-  deleteSelectedNotes: () =>
-    set((store) => ({
-      ...store,
-      selectedNotes: [],
-      notes: store.notes.filter((note) => !note.isSelected),
-    })),
-}));
+    {
+      name: 'notes-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+      onRehydrateStorage: () => (state) => {
+        state!.setHasHydrated(true);
+      },
+    },
+  ),
+);
